e0745ab0821c9f9d01c69e57e5d95e1323533c00
Azumics <70793428+cedricmatthewverdida@users.noreply.github.com>
2023-01-27 13:18:34 +0800

    Update PostMain.js

--- src/components/post/PostMain.js
+++ src/components/post/PostMain.js
@@ -1,123 +1,69 @@
 import React from 'react'
+import moment from 'moment'
+import { navigate } from "@reach/router";
 import ReactMarkdown from 'react-markdown'
 
-import Lottie from "react-lottie";
-import shapeAnimation from '../../assets/json/shape.json'
-import emptyProfile from '../../assets/images/empty-profile.png'
-import Comment from '../../assets/images/Comment.png'
-
-const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: shapeAnimation,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
-  
 
 const PostMain = ({ newsfeedData }) => {
     return (
         <div className="flex h-screen">
             <div className="m-auto">
                 <section>
-                    <div
-                        className=" max-w-screen-xl py-16 sm:px-6"
-                        style={{ borderColor: "red" }}
-                    >
-                        <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
-                            <div className="relative h-auto overflow-hidden rounded-lg sm:h-full lg:order-last lg:h-full">
-                                <div className="flex items-center justify-center">
-                                    <div style={{ zIndex: 10 }}>
-                                        <div className="mockup-phone border-primary ">
-                                            <div className="camera"></div>
-                                            <div className="display">
-                                                <div className="artboard artboard-demo phone-1"
-                                                    style={{
-                                                        backgroundImage: `url(${Comment})`,
-                                                        backgroundSize: "cover",
-                                                        backgroundRepeat: "no-repeat, repeat",
-
-                                                    }}
-                                                >
-                                                    <div
-                                                        style={{
-                                                            position: 'absolute',
-                                                            top: '50%',
-                                                            height: "40%",
-                                                            width: '60%',
-                                                        }}
-                                                    >
-                                                        <div className="flex flex-col h-full overflow-hidden">
-                                                            <main className="flex-1 overflow-y-auto">
-                                                                <div className="flex flex-col w-full">
-
-                                                                    {
-                                                                        newsfeedData?.comments?.map((comment, key) => (
-
-                                                                            <div className="mt-5 pl-4" key={key}>
-                                                                                <div className="flex flex-row p-2 items-baseline">
-                                                                                    <div className="basis-1/5">
-                                                                                        <div className="avatar">
-                                                                                            <div className="w-10 rounded-full">
-                                                                                                <img src={comment.user_image || emptyProfile} />
-                                                                                            </div>
-                                                                                        </div>
-                                                                                    </div>
-                                                                                    <div className="basis-1/1">
-
-                                                                                        <div className="card w-56 bg-base-200 card-compact">
-                                                                                            <div className="card-body">
-                                                                                                <strong className="card-title text-white">{comment.user_name}</strong>
-                                                                                                <p className="text-white-400 text-xs font-givenget">
-                                                                                                    {comment.comment}
-                                                                                                </p>
-                                                                                            </div>
-                                                                                        </div>
-
-                                                                                    </div>
-                                                                                </div>
-                                                                            </div>
-
-                                                                        ))
-                                                                    }
-
-
-                                                                </div>
-                                                            </main>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div style={{ position: "absolute" }}>
-                                        <Lottie
-                                            options={defaultOptions}
-                                            height="100%"
-                                            width="100%"
-                                            isClickToPauseDisabled={true}
-                                            style={{ cursor: "default" }}
-                                        />
-                                    </div>
-                                </div>
+                    <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+                        <div className="max-w-3xl">
+                            <div className={newsfeedData?.title.length >= 30 ? 'w-1/1' : 'w-1/2'}>
+                                <h2 className={newsfeedData?.title.length >= 30 ? `text-3xl font-bold font-givenget sm:text-3xl text-white` : `text-3xl font-bold font-givenget sm:text-5xl text-white` }>
+                                    {newsfeedData?.title}
+                                </h2>
                             </div>
 
-                            <div className="lg:py-2">
-                                <h1 className="text-4xl font-bold font-givenget sm:text-7xl text-white">
-                                    {newsfeedData?.title}
-                                </h1>
-                                <div className="mt-4">
-                                    {/* {truncateString(challengeData?.description, 500)} */}
-                                    <ReactMarkdown children={newsfeedData?.content} />
-                                </div>
+                            <div className='flex flex-row'>
+                                <span className="badge badge-lg mt-3 mr-2 font-givenget">
+                                    <div className="mr-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
+                                        </svg>
+                                    </div>
 
+                                    {newsfeedData?.created_at_web}
+                                </span>
+                                {
+                                    newsfeedData?.trending === 1 && (
+                                        <span className="badge badge-lg badge-error mt-3 font-givenget">
+                                            <div className="mr-1">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z" />
+                                                </svg>
+                                            </div>
+                                            Trending
+                                        </span>
+                                    )
+                                }
+                            </div>
+
+                        </div>
+
+                        <div className="mt-4 grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+
+                            <div className="relative h-64 overflow-hidden sm:h-80 lg:h-full">
+                                <img
+                                    alt={newsfeedData?.title}
+                                    src={newsfeedData?.photo}
+                                    className="absolute inset-0 h-full w-full object-cover rounded-3xl"
+                                />
+                            </div>
+
+                            <div className="lg:py-16">
+                                <article className="space-y-4 text-gray-600">
+                                    <ReactMarkdown children={newsfeedData?.content} />
+                                </article>
                                 <div className="flex flex-row mt-5 gap-x-5 items-center ">
                                     <div>
                                         <div
                                             style={{ cursor: "pointer" }}
                                             className="flex mt-3 w-auto p-5 h-14 bg-black text-white rounded-xl items-center justify-center transition hover:rotate-2 hover:scale-110"
+                                            onClick={() => navigate('https://apps.apple.com/us/app/give-get/id1569561799')}
                                         >
                                             <div className="mr-3">
                                                 <svg viewBox="0 0 384 512" width={30}>
@@ -140,6 +86,7 @@ const PostMain = ({ newsfeedData }) => {
                                         <div
                                             style={{ cursor: "pointer" }}
                                             className="flex mt-3 w-auto p-5 h-14 bg-black text-white rounded-lg items-center justify-center transition hover:-rotate-2 hover:scale-110"
+                                            onClick={() => navigate('https://play.google.com/store/apps/details?id=com.givenget')}
                                         >
                                             <div className="mr-3">
                                                 <svg viewBox="30 336.7 120.9 129.2" width={30}>
